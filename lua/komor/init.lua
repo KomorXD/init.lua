ca47d21575fa2288e61c8451d5c79b46bf9f1d60
KomorXD <komorxd_t@proton.me>
2024-06-09 16:26:39 +0200

    Lint actually here, support filesystem suggestions

--- lua/komor/init.lua
+++ lua/komor/init.lua
@@ -1,3 +1,11 @@
 require("komor.set")
 require("komor.remap")
 require("komor.lazy_init")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
